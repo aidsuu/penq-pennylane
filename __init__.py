77219665da268ef1c9eea58519f311a41e01eadb
aidsuu <asptr00@gmail.com>
2026-04-13 09:30:36 +0700

    feat(algorithms): add TFIM imaginary-time and real-time solvers

--- __init__.py
+++ __init__.py
@@ -4,10 +4,18 @@ from .qml_starter_device import QMLStarterDevice
 from .qml_mps_device import QMLMPSDevice
 from .penq_algorithms import adaptive_tfim_vqe
 from .penq_algorithms import compare_tfim_vqe_exact_vs_mps
+from .penq_algorithms import imaginary_time_tfim
+from .penq_algorithms import compare_imag_time_exact_vs_mps
+from .penq_algorithms import real_time_tfim
+from .penq_algorithms import compare_real_time_exact_vs_mps
 
 __all__ = [
     "QMLStarterDevice",
     "QMLMPSDevice",
     "adaptive_tfim_vqe",
     "compare_tfim_vqe_exact_vs_mps",
+    "imaginary_time_tfim",
+    "compare_imag_time_exact_vs_mps",
+    "real_time_tfim",
+    "compare_real_time_exact_vs_mps",
 ]
